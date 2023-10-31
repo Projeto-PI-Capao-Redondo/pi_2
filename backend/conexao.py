9786777a10dabb3aea8ddf6a09aed625529ae412
Igor Esposito <sposigor@gmail.com>
2023-10-30 22:29:45 -0300

    heroku

--- backend/conexao.py
+++ backend/conexao.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import Session, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL_HEROKU = os.getenv('DATABASE_URL_HEROKU')
+string_conexao = str(os.getenv('DATABASE_URL_HEROKU'))
 
-engine = create_engine(DATABASE_URL_HEROKU)
+engine = create_engine(string_conexao)
 
 def get_session() -> Session:
     session = sessionmaker(bind=engine)

8aa6e0bb0523903010d3f140db3c5fffbabaeda8
Igor Esposito <sposigor@gmail.com>
2023-10-30 22:05:06 -0300

    heroku

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,11 @@
+import os
+
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+DATABASE_URL_HEROKU = os.getenv('DATABASE_URL_HEROKU')
 
 
 class Settings(BaseSettings):
